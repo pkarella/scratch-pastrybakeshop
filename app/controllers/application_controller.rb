c6c399512c9b5ec68a74cb9cfe420d72ee63838d
peter <pkarella27@yahoo.com>
2017-08-05 12:03:28 -0700

    styling for footer and section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :password) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :password,:avatar) }
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :username, :password, :current_password, :avatar) }
   end
 end
